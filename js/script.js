c6a2ccdd5ec2903fb9ce6be97a4279d536bfe4a6
onnyx1 <spencer.retcher1@gmail.com>
2022-08-26 01:31:23 -0400

    Completed Project

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const password = document.getElementById("password");
+const confirm = document.getElementById("confirmPassword");
+const errorLabel = document.getElementById("errorLabel");
+
+function check() {
+  if (password.value === confirm.value) {
+    password.classList.remove("error");
+    confirm.classList.remove("error");
+    confirm.setCustomValidity("");
+  } else {
+    password.classList.add("error");
+    confirm.classList.add("error");
+    errorLabel.removeAttribute("hidden");
+    confirm.setCustomValidity("Passwords do not match");
+  }
+}
